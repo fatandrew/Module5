41dc187796ec2a9b475ec8fda0e09741ebf388cd
Andrew Danylenko <wdanylenko@gmail.com>
2016-03-13 14:49:42 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = {1, 3, 4, 55677, 8, -987};
+        int min = intArray[0];
+        for (int i = 0; i < intArray.length; i++) {
+            if (min > intArray[i])
+                min = intArray[i];
+        }
+        System.out.println("Minimum variable is: " + min);
+
+        int max = intArray[0];
+        for (int i = 0; i < intArray.length; i++) {
+            if (max < intArray[i])
+                max = intArray[i];
+        }
+        System.out.println("Maximum variable is: " + max);
+
+        Arrays.sort(intArray);
+        for (int element : intArray) {
+            System.out.println(element);
+        }
+    }
+}
+
