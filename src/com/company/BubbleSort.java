fb73ebb76fecf315346bf0f1377f8e882dc11e90
Andrew Danylenko <wdanylenko@gmail.com>
2016-03-23 00:43:05 +0200

    Add exception

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -5,26 +5,33 @@ package com.company;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        int number[] = {8, 7, 25, 6, 55, 1, 3, 5};
-        int temp;
-        boolean fixed = false;
+        try {
+            int number[] = {8, 7, 25, 6, 55, -5, 211, 23};
+            int temp;
+            boolean fixed = false;
 
-        while (fixed == false) {
+            while (fixed == false) {
 
-            fixed = true;
-            for (int i = 0; i < number.length - 1; i++) {
-                if (number[i] > number[i + 1]) {
-                    temp = number[i + 1];
-                    number[i + 1] = number[i];
-                    number[i] = temp;
-                    fixed = false;
+                fixed = true;
+                for (int i = 0; i < number.length - 1; i++) {
+                    if (number[i] > number[i + 1]) {
+                        temp = number[i + 1];
+                        number[i + 1] = number[i];
+                        number[i] = temp;
+                        fixed = false;
+                    }
                 }
-
             }
-        }
+            if (number.length > 7) {
+                throw new IllegalArgumentException("The quantity of numbers cannot be more then 7");
+            }
             for (int i = 0; i < number.length; i++) {
                 System.out.println(number[i]);
             }
 
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
